4081e955742aa9d54b7176e276935f105c869188
Alaric-Gautier <92737965+Alaric-Gautier@users.noreply.github.com>
2022-05-13 18:10:05 +0200

    base and home page done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,11 @@
 
 import '../css/app.scss';
 
+import {Dropdown} from "bootstrap";
+
+const enableDropdown = () => {
+    const dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'))
+    dropdownElementList.map(function (dropdownToggleEl) {
+        return new Dropdown(dropdownToggleEl)
+    });
+}
