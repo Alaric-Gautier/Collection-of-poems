7c6879c96a71a7134c1188505dabc6aefbabcd76
Alaric-Gautier <92737965+Alaric-Gautier@users.noreply.github.com>
2022-06-22 14:25:37 +0200

    ajax request ok, view ok

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,6 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
+use App\Repository\CommentRepository;
+use App\Repository\PoemRepository;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,12 +15,40 @@ use Symfony\Component\Routing\Annotation\Route;
 class CommentController extends AbstractController
 {
     #[Route('/ajax/comments', name: 'comment_add')]
-    public function add(Request $request): Response
+    public function add(Request $request, PoemRepository $poemRepository, EntityManagerInterface $entityManager,UserRepository $userRepository, CommentRepository $commentRepository): Response
     {
         $commentData = $request->request->all('comment');
 
-        return $this->render('comment/index.html.twig', [
-            'controller_name' => 'CommentController',
+        if (!$this->isCsrfTokenValid('comment-add', $commentData['_token'])) {
+            return $this->json([
+                'code' => 'INVALID_CSRF_TOKEN'
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        $poem = $poemRepository->findOneBy(['id' => $commentData['poem']]);
+
+        if (!$poem) {
+            return $this->json([
+                'code' => 'POEM_NOT_FOUND'
+            ], Response::HTTP_BAD_REQUEST);
+        }
+
+        $comment = new Comment($poem);
+        $comment->setContent($commentData['content']);
+        $comment->setUser($userRepository->findOneBy(['id' => 1]));
+        $comment->setCreatedAt(new \DateTime());
+
+        $entityManager->persist($comment);
+        $entityManager->flush();
+
+        $html = $this->renderView('comment/index.html.twig', [
+            'comment' => $comment
+        ]);
+
+        return $this->json([
+            'code' => 'COMMENT_ADDED_SUCCESSFULLY',
+            'message' => $html,
+            'numberOfComments' =>$commentRepository->count(['poem' => $poem])
         ]);
     }
 }
